9e9d356f5ab2b021a66bb6b8110fda1fe54ef90f
Sandro Loch <es.loch@gmail.com>
2020-11-23 14:50:21 -0300

    Change variables to download dev_dumps

--- docker/download_dumps.sh
+++ docker/download_dumps.sh
@@ -8,4 +8,4 @@ set -a
 
 wget --user ${DB_USER} --password ${DB_PASSWORD} -c https://info.dengue.mat.br/dev_dumps/dengue.sql.gz -O docker/dev_dumps/latest_dengue.sql.gz
 
-wget --user ${DB_USER} --password  ${DB_PASSWORD} -c https://info.dengue.mat.br/dev_dumps/infodengue.sql.gz -O docker/dev_dumps/latest_infodengue.sql.gz
+wget --user ${DB_USER} --password ${DB_PASSWORD} -c https://info.dengue.mat.br/dev_dumps/infodengue.sql.gz -O docker/dev_dumps/latest_infodengue.sql.gz
